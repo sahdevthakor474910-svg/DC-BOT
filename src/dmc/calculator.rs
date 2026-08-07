a037745e6f6a3ec2b4f03dbbd0200fbb26bfd574
SahdevXD <thakorsahdev95@gmail.com>
2026-08-08 02:50:32 +0530

    dmc: add Beowulf HP 5,783,842,000 (same group as Plutone, game HP 5.078B x 1.139)

--- src/dmc/calculator.rs
+++ src/dmc/calculator.rs
@@ -18,7 +18,8 @@ fn boss_dmg_pts(name: &str) -> i64 {
     if norm.contains("vergil")
         || norm.contains("dante")            { 2_892_440_140 } // kept at pre-buff value until confirmed
     else if norm.contains("lady")            { 9_038_840_000 } // 5-min boss
-    else if norm.contains("plutone")         { 5_783_842_000 } // calibrated from results screen
+    else if norm.contains("plutone")
+        || norm.contains("beowulf")          { 5_783_842_000 } // game HP 5.078B × 1.139 score mul
     else                                     { 5_070_000_000 } // all other bosses (incl. HC post-buff)
 }
 
